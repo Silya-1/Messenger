fadf613e89e0a20164eaf7a3f49a4e8d693995bc
Silya-1 <ilja5050@yandex.ru>
2016-10-04 21:49:26 +0300

    first commit

---
+++ src/test/main.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Silya on 04.10.16.
+ */
+public class main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
